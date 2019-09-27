8fdbff844a175e041af6049bd048c7c2cbd99ccf
SyntaxSeed <825423+syntaxseed@users.noreply.github.com>
2019-09-27 11:43:35 -0400

    Update PHPUnit and failing tests.

--- tests/TemplateSeedTest.php
+++ tests/TemplateSeedTest.php
@@ -2,13 +2,13 @@
 /**
 *  Basic testing for TemplateSeed.
 */
-class TemplateSeedTest extends PHPUnit_Framework_TestCase{
+class TemplateSeedTest extends PHPUnit\Framework\TestCase{
 
     /**
     * Basic test of class instantiation.
     */
     public function testIsThereAnySyntaxErrors(){
-    	$tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+    	$tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
     	$this->assertTrue(is_object($tpl));
     	unset($tpl);
     }
@@ -17,7 +17,7 @@ class TemplateSeedTest extends PHPUnit_Framework_TestCase{
     * Check if retrieving a template works.
     */
     public function testTemplateRetrieval(){
-        $tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+        $tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
         $tpl->setTemplate('greeting');
         $tpl->params->name = "Keegan";
         $output = $tpl->retrieve();
@@ -29,7 +29,7 @@ class TemplateSeedTest extends PHPUnit_Framework_TestCase{
     * Check if retrieving a template with a global parameter works.
     */
     public function testTemplateGlobalRetrieval(){
-        $tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+        $tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
         $tpl->setTemplate('greetingglobal');
         $tpl->setGlobalParams(['first'=>'Keegan']);
         $tpl->params->last = "Smith";
@@ -42,7 +42,7 @@ class TemplateSeedTest extends PHPUnit_Framework_TestCase{
     * Check one line render method.
     */
     public function testTemplateRender(){
-        $tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+        $tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
         $output = $tpl->render('greeting', ['name'=>'Keegan']);
     	$this->assertEquals($output, "<h1>Well hello there, Keegan!</h1>\n");
         unset($tpl);
@@ -52,10 +52,10 @@ class TemplateSeedTest extends PHPUnit_Framework_TestCase{
     * Check the Safe String helper.
     */
     public function testTemplateHelperSS(){
-        $tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+        $tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
         $output = $tpl->render('helpers');
-        $this->assertContains('Helpers:', $output);
-        $this->assertContains('&amp;', $output);
+        $this->assertStringContainsString('Helpers:', $output);
+        $this->assertStringContainsString('&amp;', $output);
         unset($tpl);
     }
 
@@ -63,10 +63,10 @@ class TemplateSeedTest extends PHPUnit_Framework_TestCase{
     * Check the View helper.
     */
     public function testTemplateHelperView(){
-        $tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+        $tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
         $output = $tpl->render('helpers');
-        $this->assertContains('Helpers:', $output);
-        $this->assertContains('Freeman', $output);
+        $this->assertStringContainsString('Helpers:', $output);
+        $this->assertStringContainsString('Freeman', $output);
         unset($tpl);
     }
 
@@ -74,10 +74,10 @@ class TemplateSeedTest extends PHPUnit_Framework_TestCase{
     * Check the $_tpl helper var is included.
     */
     public function testTemplateHelperTpl(){
-        $tpl = new syntaxseed\templateseed\TemplateSeed(__DIR__."/views/");
+        $tpl = new Syntaxseed\Templateseed\TemplateSeed(__DIR__."/views/");
         $output = $tpl->render('helpers');
-        $this->assertContains('Helpers:', $output);
-        $this->assertContains('Syntaxseed\Templateseed\TemplateSeed', $output);
+        $this->assertStringContainsString('Helpers:', $output);
+        $this->assertStringContainsString('Syntaxseed\Templateseed\TemplateSeed', $output);
         unset($tpl);
     }
 }
